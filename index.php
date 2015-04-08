15e465a1d723d3d03b2175088ab3f8aa26cdc7ce
LukeKing313 <lucas.assink97@gmail.com>
2015-04-08 20:13:13 +0200

    basis
    basic html and css

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Anime</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<h3 class="mark">AUTISME & CO</h3>
+		<h3>register</h3>
+		<h3>login</h3>	
+	</header>
+		<ul>
+			<li><a href="#">HOME</a></li>
+			<li><a href="#">TOPICS</a></li>
+			<li><a href="#">CONTACT</a></li>
+		</ul>
+
+</body>
+</html>
