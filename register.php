bf5574abdb2342259fbb6eb8f5222cdf0e1cc2b3
LukeKing313 <lucas.assink97@gmail.com>
2015-04-09 14:19:25 +0200

    merge cleanup
    sugar honey ice tea

--- register.php
+++ register.php
@@ -12,19 +12,8 @@
 <?php require'small_codebehind/header.php' ?>
 	<div class="register">
 		<form method="post">
-			<span><div class="namepass">Username: </div><input type="text" name="username"></span><br>
+			<span><div class="namepass"> Username: </div><input type="text" name="username"></span><br>
 			<span><div class="namepass">Password: </div><input type="password" name="password"></span><br>
-<<<<<<< HEAD
-			<span>Email adress: <input type="email" name="email"></span><br>
-			<span>Age: <select name="age">
-						<option>Under 13</option>
-						<option>Between 13 and 18</option>
-						<option>18 or above</option>
-					   </select>
-			</span><br>
-			<span>First name: <input type="text" name="first_name"></span><br>
-			<span>Last name: <input type="text" name="last_name"></span><br>
-=======
 			<span><div class="namepass">Email adress: </div><input type="text" name="email"></span><br>
 			<span><div class="namepass">First name: </div><input type="text" name="first_name"></span><br>
 			<span><div class="namepass">Last name: </div><input type="text" name="last_name"></span><br>
@@ -34,7 +23,6 @@
 					<option>18 or above</option>
 				</select>
 			</span><br><br>
->>>>>>> origin/master
 			<span><input class="button" type="submit" value="Send"/></span>
 	</form>
 </div>
