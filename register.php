87e82328a79678d90be82dc7bcae0c135dcaa57a
cbr600f0 <sidneybatenburg@hotmail.com>
2015-04-09 13:30:51 +0200

    register
    changed some shizzle ma nizzle

--- register.php
+++ register.php
@@ -4,13 +4,15 @@
 	<title>Register</title>
 	<link rel="icon" type="image/jpg" href="images/eye.png">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
-	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<link rel="stylesheet" type="text/css" href="css/account.css">
 </head>
 <body>
 <?php require'small_codebehind/header.php' ?>
-	<form method="post">
-		<span>Username: <input type="text" name="username"></span><br>
-		<span>Password: <input type="password" name="password"></span><br>
-	</form>
+	<div class="register">
+		<form method="post">
+			<span><div class="namepass">Username: </div><input type="text" name="username"></span><br>
+			<span><div class="namepass">Password: </div><input type="password" name="password"></span><br>
+		</form>
+	</div>
 </body>
 </html>
