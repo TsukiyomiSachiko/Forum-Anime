fd998b0c7cbf217261bc372661da5d823509beb5
JRoffel <jasonroffel@hotmail.nl>
2015-04-09 14:01:21 +0200

    Version 0.0.2
    Hello

--- register.action.php
+++ register.action.php
@@ -1,5 +1,9 @@
 <?php
-	$connection = new mysqli('localhost', 'root', '', 'anime');
-	$query = '';
-	$result = $connection -> query($query);
-	$row = $result -> fetch_all(MYSQLI_ASSOC);
+
+	if(isset($_POST['username']) and isset($_POST['password']) and isset($_POST['email']) and isset($_POST['age']) and isset($_POST['first_name']) and isset($_POST['last_name'])):
+		$connection = new mysqli('localhost', 'root', '', 'anime');
+		$query = '';
+		$result = $connection -> query($query);
+		$row = $result -> fetch_all(MYSQLI_ASSOC);
+	endif;
+
