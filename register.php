c2d6d4e60c532cbe06870002c80e8bdb9bec9546
cbr600f0 <sidneybatenburg@hotmail.com>
2015-04-09 14:00:18 +0200

    css
    kawaiiiiiiiiiiiiii

--- register.php
+++ register.php
@@ -12,15 +12,15 @@
 		<form method="post">
 			<span><div class="namepass">Username: </div><input type="text" name="username"></span><br>
 			<span><div class="namepass">Password: </div><input type="password" name="password"></span><br>
-			<span>Email adress: <input type="text" name="email"></span><br>
-			<span>Age: <select name="age">
-						<option>Under 13</option>
-						<option>Between 13 and 18</option>
-						<option>18 or above</option>
-					   </select>
-			</span><br>
-			<span>First name: <input type="text" name="first_name"></span><br>
-			<span>Last name: <input type="text" name="last_name"></span><br>
+			<span><div class="namepass">Email adress: </div><input type="text" name="email"></span><br>
+			<span><div class="namepass">First name: </div><input type="text" name="first_name"></span><br>
+			<span><div class="namepass">Last name: </div><input type="text" name="last_name"></span><br>
+			<span><div class="namepass">Age: </div><select name="age">
+					<option>Under 13</option>
+					<option>Between 13 and 18</option>
+					<option>18 or above</option>
+				</select>
+			</span><br><br>
 			<span><input class="button" type="submit" value="Send"/></span>
 	</form>
 </div>
