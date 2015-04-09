21c15f943f79ed77e42ae8033d9dc9f1f14a484e
LukeKing313 <lucas.assink97@gmail.com>
2015-04-09 11:04:17 +0200

    actions.1
    more kay

--- index.action.php
+++ index.action.php
@@ -1,6 +1,6 @@
 <?php 
-	//$connection = new mysqli('localhost', 'root', '', '');
-	//$query = ""
-	//$result = $connection->query($query);
-	//$games = $result->fetch_all(MYSQLI_ASSOC);	
+	$connection = new mysqli('localhost', 'root', '', 'anime');
+	$query = ""
+	$result = $connection->query($query);
+	$games = $result->fetch_all(MYSQLI_ASSOC);	
 ?>
