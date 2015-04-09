addb0b48aa58d02bb9bb9cf1a3cbfb657c6d14f2
LukeKing313 <lucas.assink97@gmail.com>
2015-04-09 13:33:20 +0200

    fixed merge
    got rid of trash

--- register.php
+++ register.php
@@ -8,30 +8,21 @@
 </head>
 <body>
 <?php require'small_codebehind/header.php' ?>
-<<<<<<< HEAD
 	<div class="register">
 		<form method="post">
 			<span><div class="namepass">Username: </div><input type="text" name="username"></span><br>
 			<span><div class="namepass">Password: </div><input type="password" name="password"></span><br>
-		</form>
-	</div>
-=======
-<div class="register">
-	<form method="post">
-		<span>Username: <input type="text" name="username"></span><br>
-		<span>Password: <input type="password" name="password"></span><br>
-		<span>Email adress: <input type="text" name="email"></span><br>
-		<span>Age: <select name="age">
-					<option>Under 13</option>
-					<option>Between 13 and 18</option>
-					<option>18 or above</option>
-				   </select>
-		</span><br>
-		<span>First name: <input type="text" name="first_name"></span><br>
-		<span>Last name: <input type="text" name="last_name"></span><br>
-		<span><input class="button" type="submit" value="Send"/></span>
+			<span>Email adress: <input type="text" name="email"></span><br>
+			<span>Age: <select name="age">
+						<option>Under 13</option>
+						<option>Between 13 and 18</option>
+						<option>18 or above</option>
+					   </select>
+			</span><br>
+			<span>First name: <input type="text" name="first_name"></span><br>
+			<span>Last name: <input type="text" name="last_name"></span><br>
+			<span><input class="button" type="submit" value="Send"/></span>
 	</form>
 </div>
->>>>>>> origin/master
 </body>
 </html>
