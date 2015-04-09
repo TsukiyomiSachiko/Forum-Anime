caaba69474c5b1c21783459782bfc23a3f663640
LukeKing313 <lucas.assink97@gmail.com>
2015-04-09 11:27:10 +0200

    fix
    .

--- index.action.php
+++ index.action.php
@@ -1,13 +1,6 @@
 <?php 
-<<<<<<< HEAD
-	//$connection = new mysqli('localhost', 'root', '', 'anime');
-	//$query = ""
-	//$result = $connection->query($query);
-	//$games = $result->fetch_all(MYSQLI_ASSOC);	
-=======
 	$connection = new mysqli('localhost', 'root', '', 'anime');
 	$query = "";
 	$result = $connection->query($query);
 	$games = $result->fetch_all(MYSQLI_ASSOC);	
->>>>>>> origin/master
 ?>
