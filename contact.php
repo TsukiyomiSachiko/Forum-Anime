6a36775f869586509c07f4a8cbae89cc0a9eff0d
LukeKing313 <lucas.assink97@gmail.com>
2015-04-10 17:58:02 +0200

    Contact & Main.css DONE
    css for the contact page is done

--- contact.php
+++ contact.php
@@ -8,29 +8,30 @@
 </head>
 <body>
 <?php require'small_codebehind/header.php' ?>
-	<div class="form">
-		<h1>Contact one of the admins and we'll try to respond as soon as possible</h1>
-		<form method="post" action="contact.php">
-		<label>Name: </label><br>
-		<input class="input" type="text" name="name" value="">
-		<span class"error"><?php echo $nameError; ?></span><br><br>
+	<div class="contact_container">
+		<div class="contact_form">
+			<h1 class="contact_h1">Contact one of the admins and we'll try to respond as soon as possible</h1><hr/><br>
+			<form method="post" action="contact.php">
+			<label class="contact_label">Name: </label><br>
+			<input class="contact_input" type="text" name="name" value="">
+			<span class="contact_error"><?php echo $nameError; ?></span><br><br>
 
-		<label>Email :</label><br>
-		<input class="input" type="text" name="email" value="">
-		<span class"error"><?php echo $emailError; ?></span><br><br>
-		
-		<label>Title :</label><br>
-		<input class="input" type="text" name="title" value="">
-		<span class"error"><?php echo $titleError; ?></span><br><br>
+			<label class="contact_label">Email :</label><br>
+			<input class="contact_input" type="text" name="email" value="">
+			<span class="contact_error"><?php echo $emailError; ?></span><br><br>
+			
+			<label class="contact_label">Title :</label><br>
+			<input class="contact_input" type="text" name="title" value="">
+			<span class="contact_error"><?php echo $titleError; ?></span><br><br>
 
-		<label>Message :</label><br>
-		<input class="input" type="text" name="message" value="">
-		<span class"error"><?php echo $messageError; ?></span><br><br>
+			<label class="contact_label">Message :</label><br>
+			<textarea class="contact_message_input" name="message" value=""></textarea>
+			<span class="contact_error"><?php echo $messageError; ?></span><br><br>
 
-		<input class="submit" type="submit" name="submit" value="submit">
-		<span class"succes"><?php echo $successMessage; ?></span><br><br>
-
-		</form>
+			<input class="contact_submit" type="submit" name="submit" value="submit">
+			<span class="contact_succes"><?php echo $successMessage; ?></span><br><br>
+			</form>
+		</div>
 	</div>
 <?php require'small_codebehind/footer.php' ?>
 </body>
