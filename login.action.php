99e3c6a33bf7cac8317ca62bf3a06e6307795f6d
daerkone <timocasper@msn.com>
2015-04-13 09:47:43 +0200

    login puntjes op i
    $log heeft nu strings!!

--- login.action.php
+++ login.action.php
@@ -1,21 +1,24 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == "POST") :
 $connection = new mysqli('localhost', 'root', '', 'anime');
-	$password = $_POST['pass'];
+	$password = $_POST['password'];
 	$query = 'SELECT * FROM users';
 	$result = $connection->query($query);
 	$alluser = $result->fetch_all(MYSQLI_ASSOC);
 	foreach	($alluser as $quser) :
-		if ($quser['username'] == $_POST['user'])
+		if ($quser['username'] == $_POST['username'])
 		{
-			if (password_verify($_POST['pass'], $quser['password'])){
+			if (password_verify($password, $quser['password'])){
 				$_SESSION['rank'] = $quser['rank'];
 				$_SESSION['login'] = $quser['login'];
-				$log = true;
+				$log = 'true';
 				break ; }
+				else {
+					$log = 'passfalse';
+				}
 		} else
 		{
-			$log = false;
+			$log = 'userfalse';
 		}
 	endforeach;
 header('location:index.php?log='.$log);
