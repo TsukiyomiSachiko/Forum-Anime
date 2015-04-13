47303086cbf91a232763e843c33ea89b3ca321c6
LukeKing313 <lucas.assink97@gmail.com>
2015-04-13 09:30:21 +0200

    autistischsjdbas

--- register.php
+++ register.php
@@ -13,16 +13,28 @@
 		<div class="register_form">
 		<h1 class="register_h1">Make your account here</h1><hr/><br>
 			<form method="post" action="confirm.php">
-				<label class="register_label">Username: </label><br><input class="register_input" type="text" name="username"><br>
-				<label class="register_label">Password: </label><br><input class="register_input" type="password" name="password"><br>
-				<label class="register_label">Email adress: </label><br><input class="register_input" type="text" name="email"><br>
-				<label class="register_label">First name: </label><br><input class="register_input" type="text" name="first_name"><br>
-				<label class="register_label">Last name: </label><br><input class="register_input" type="text" name="last_name"><br>
-				<label class="register_label">Age: </label><br><select class="register_select" name="age">
-						<option>Under 13</option>
-						<option>Between 13 and 18</option>
-						<option>18 or above</option>
-					</select>
+				<label class="register_label">Username: </label><br>
+				<input class="register_input" type="text" name="username"><br>
+
+				<label class="register_label">Password: </label><br>
+				<input class="register_input" type="password" name="password"><br>
+
+				<label class="register_label">Email adress: </label><br>
+				<input class="register_input" type="text" name="email"><br>
+
+				<label class="register_label">First name: </label><br>
+				<input class="register_input" type="text" name="first_name"><br>
+
+				<label class="register_label">Last name: </label><br>
+				<input class="register_input" type="text" name="last_name"><br>
+
+				<label class="register_label">Age: </label><br>
+				<select class="register_select" name="age">
+					<option>Under 13</option>
+					<option>Between 13 and 18</option>
+					<option>18 or above</option>
+				</select>
+
 				<br><br>
 				<input class="register_submit" type="submit" value="Send"/>
 			</form>
