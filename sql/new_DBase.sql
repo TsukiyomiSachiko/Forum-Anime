3e2b01d475ca5e4ca105553076b1eb45bd3b2c69
JRoffel <jasonroffel@hotmail.nl>
2015-04-13 09:07:19 +0200

    Revert "SQ NIGGA'S"
    This reverts commit 815b4d69bb2dda0f63694b4ac3b598458b5257e4.

--- sql/new_DBase.sql
+++ sql/new_DBase.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Server version:               5.6.17 - MySQL Community Server (GPL)
+-- Server versie:                5.6.17 - MySQL Community Server (GPL)
 -- Server OS:                    Win64
--- HeidiSQL Version:             9.1.0.4867
+-- HeidiSQL Versie:              9.1.0.4867
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -10,31 +10,29 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Dumping database structure for anime
+-- Databasestructuur van anime wordt geschreven
 DROP DATABASE IF EXISTS `anime`;
 CREATE DATABASE IF NOT EXISTS `anime` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `anime`;
 
 
--- Dumping structure for table anime.adminboard
+-- Structuur van  tabel anime.adminboard wordt geschreven
 DROP TABLE IF EXISTS `adminboard`;
 CREATE TABLE IF NOT EXISTS `adminboard` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(250) NOT NULL DEFAULT '0',
   `date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `user_id` int(11) NOT NULL DEFAULT '0',
-  `title` varchar(1000) NOT NULL,
-  `comment` varchar(10000) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table anime.adminboard: ~0 rows (approximately)
+-- Dumpen data van tabel anime.adminboard: ~0 rows (ongeveer)
 DELETE FROM `adminboard`;
 /*!40000 ALTER TABLE `adminboard` DISABLE KEYS */;
 /*!40000 ALTER TABLE `adminboard` ENABLE KEYS */;
 
 
--- Dumping structure for table anime.comments
+-- Structuur van  tabel anime.comments wordt geschreven
 DROP TABLE IF EXISTS `comments`;
 CREATE TABLE IF NOT EXISTS `comments` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -45,13 +43,13 @@ CREATE TABLE IF NOT EXISTS `comments` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table anime.comments: ~0 rows (approximately)
+-- Dumpen data van tabel anime.comments: ~0 rows (ongeveer)
 DELETE FROM `comments`;
 /*!40000 ALTER TABLE `comments` DISABLE KEYS */;
 /*!40000 ALTER TABLE `comments` ENABLE KEYS */;
 
 
--- Dumping structure for table anime.news
+-- Structuur van  tabel anime.news wordt geschreven
 DROP TABLE IF EXISTS `news`;
 CREATE TABLE IF NOT EXISTS `news` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -61,13 +59,13 @@ CREATE TABLE IF NOT EXISTS `news` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table anime.news: ~0 rows (approximately)
+-- Dumpen data van tabel anime.news: ~0 rows (ongeveer)
 DELETE FROM `news`;
 /*!40000 ALTER TABLE `news` DISABLE KEYS */;
 /*!40000 ALTER TABLE `news` ENABLE KEYS */;
 
 
--- Dumping structure for table anime.topics
+-- Structuur van  tabel anime.topics wordt geschreven
 DROP TABLE IF EXISTS `topics`;
 CREATE TABLE IF NOT EXISTS `topics` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -75,17 +73,15 @@ CREATE TABLE IF NOT EXISTS `topics` (
   `date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `user_id` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table anime.topics: ~1 rows (approximately)
+-- Dumpen data van tabel anime.topics: ~0 rows (ongeveer)
 DELETE FROM `topics`;
 /*!40000 ALTER TABLE `topics` DISABLE KEYS */;
-INSERT INTO `topics` (`id`, `name`, `date`, `user_id`) VALUES
-	(1, 'Leblanc en Zed zijn 2 achtelijke mongolen die niet precies weten wat ze wel en niet kunnen doen. Daarom besluiten ze kinderen te maken, hier kwam de scout: "Teemo" uit.', '2010-06-30 21:15:15', 1);
 /*!40000 ALTER TABLE `topics` ENABLE KEYS */;
 
 
--- Dumping structure for table anime.users
+-- Structuur van  tabel anime.users wordt geschreven
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -99,14 +95,13 @@ CREATE TABLE IF NOT EXISTS `users` (
   `rank` int(11) NOT NULL DEFAULT '1',
   `join_date` timestamp NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table anime.users: ~1 rows (approximately)
+-- Dumpen data van tabel anime.users: ~0 rows (ongeveer)
 DELETE FROM `users`;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` (`id`, `username`, `password`, `first_name`, `last_name`, `email`, `phonenumber`, `age`, `rank`, `join_date`) VALUES
-	(1, 'cbr600f0', 'autist4tw', 'sidney', 'batenburg', 'sidneybatenburg@hotmail.com', 614914901, 18, 1, '2015-04-13 08:59:43');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+animeanime
