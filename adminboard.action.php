45a3769afdc1cba299c5fb13a7f3dd3d4874b8b0
LukeKing313 <lucas.assink97@gmail.com>
2015-04-13 12:25:11 +0200

    merge cleanup
    title says enough

--- adminboard.action.php
+++ adminboard.action.php
@@ -2,5 +2,5 @@
 	$connection = new mysqli('localhost', 'root', '', 'anime');
 	$query = "select name, date, user_id, title, comment from adminboard";
 	$result = $connection->query($query);
-	$games = $result->fetch_all(MYSQLI_ASSOC);	
+	$pins = $result->fetch_all(MYSQLI_ASSOC);	
 ?>
--- index.action.php
+++ index.action.php
@@ -2,9 +2,4 @@
 	$connection = new mysqli('localhost', 'root', '', 'anime');
 	$query = "select * from users";
 	$result = $connection->query($query);
-<<<<<<< HEAD
 	$news = $result->fetch_all(MYSQLI_ASSOC);
-=======
-	$news = $result->fetch_all(MYSQLI_ASSOC);	
->>>>>>> origin/master
-?>
