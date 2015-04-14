e2ed1d2848f4f8a855e833fe0336d0cb4d7e469e
cbr600f0 <sidneybatenburg@hotmail.com>
2015-04-14 09:35:16 +0200

    topic_read
    het is nu een php bestand :)

---
+++ topic_read.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Topics</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<div class="topic_row">
+		<div class="profile_short">
+		
+		</div>
+		<div class="content"> </div>
+	</div>
+</body>
+</html>
